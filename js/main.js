5307c8ec593beeedf1f2d86975ca7be96f6ae0dd
Holden McGinnis <holdenjmcginnis@gmail.com>
2015-01-12 09:10:20 -0500

    Newest Version/Things actually work now

--- js/main.js
+++ js/main.js
@@ -1,25 +1,184 @@
+var name; // Name holds current name, 
+var tableString; // Holds the whole string of the table info
+var currPlayerIndex; // Holds the index of the current player
+var college; //Holds college
+
+//Returns a boolean: true if the player is in the database
+function checkForPlayerInDatabase() {
+  var playerList = tableString.split("\n");
+  for (var i = 0; i < 100; i++) {
+    var attributes = playerList[i].split(",");
+    if (attributes[0] + " " + attributes[1] == name) {
+
+      currPlayerIndex = i;
+      college = attributes[5];
+      //console.log(college);
+      return true;
+    }
+  }
+  
+  return false;
+}
+
+//Takes the form input and loads playerpage
 function getPlayerPage() {
-  var name = document.getElementById('playername').value;
+  
+  name = document.getElementById('playername').value;
   document.getElementById('playername').value = "";
-  alert("You typed " + name);
-  parent.location = "./playerpage.html";
-  //populatePlayerPage(name);
+  //alert("You typed " + name);
+
+  if (checkForPlayerInDatabase()) {
+    alert(name + " is in database.");
+    parent.location = "./playerpage.html";
+  } else {
+    alert(name + " wasn't in database, try again please!");
+  }
+
+  
 }
 
-function populatePlayerPage(name) {
-  alert("You called populatePlayerPage");
-  //Needs to populate the elements of playerpage.html
+//Populates the elements of the player profile
+function populatePlayerProfile() {
+  //alert("You called populatePlayerProfile");
 
+  $('h1').text(name);
+  var playerList = tableString.split("\n");
+  var attributes = playerList[currPlayerIndex].split(",");
+  college = attributes[5];
+  var profilebox = $('.profile-box').children().first().next();
+  profilebox.text("Height: " + attributes[2]);
+  profilebox.next().text("Weight: " + attributes[3]);
+  profilebox.next().next().text("Position: " + attributes[4]);
+  profilebox.next().next().next().text("College/Country: " + attributes[5]);
+  profilebox.next().next().next().next().text("Year: " + attributes[6]);
+  $(".headline-box").height($(".profile-box").height());
+  $(".video-box").height($(".profile-box").height());
 
+}
+//Adds the photo and resizes it
+function addPlayerPhoto(photoURL, height, width) {
+  $('.profile-box .profile-top img').attr("src", photoURL);
+  var aspectRatio = 250/width;
+  $('.profile-box .profile-top img').attr("height", aspectRatio*height);
+  $('.profile-box .profile-top img').attr("width", aspectRatio*width);
 
 }
 
-
+//Loads the players page from the table click
 function getPlayerPageFromTable(firstname, lastname) {
-  var name = firstname + " " + lastname;
-  alert("You clicked on " + name);
+  name = firstname + " " + lastname;
+  //alert("You clicked on " + name);
+  if (checkForPlayerInDatabase()){
+    console.log("getPlayerPageFromTable says player in directory");
+    parent.location = "./playerpage.html";
+  } else {
+    console.log("getPlayerPageFromTable says player NOT in directory");
+  }//Used to set the currPlayerIndex
 
-  parent.location = "./playerpage.html";
-  populatePlayerPage(name);
+  
 }
+
+//Takes the scraped input and fills the table on the main page
+function populatePlayerIndex() {
+  var firstname, lastname, year, position, height, weight, college;
+  //console.log("Ran populatePlayerIndex");
+  playerList = tableString.split("\n");
+  var currRow, currElem;
+
+  currRow = $('tbody tr');
+
+  for (var i = 0; i < 100; i++) {
+    var attributes = playerList[i].split(",");
+
+    //currElem now holds the first element of the row
+    currElem = currRow.children().first();
+
+    currElem.text(attributes[0]);
+    currElem = currElem.next();
+    currElem.text(attributes[1]);
+    currElem = currElem.next();
+    currElem.text(attributes[6]);
+    currElem = currElem.next();
+    currElem.text(attributes[4]);
+    currElem = currElem.next();
+    currElem.text(attributes[2]);
+    currElem = currElem.next();
+    currElem.text(attributes[3]);
+    currElem = currElem.next();
+    currElem.text(attributes[5]);
+
+    currRow = currRow.next();
+
+  }
+
+}
+//Scrapes the table
+function accessTableScraper() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET","http://localhost:8001/tableScrape", true);
+  xmlhttp.onreadystatechange = function(){
+         if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+           tableString = xmlhttp.responseText;
+           playerList = tableString.split("\n");
+           populatePlayerIndex();
+           
+         }
+   }
+  xmlhttp.send();
+  
+}
+//Scrapes the internet to fill the player database info
+function profileScrape() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", "http://localhost:8001/tableScrape", true);
+  xmlhttp.onreadystatechange = function(){
+         if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+           tableString = xmlhttp.responseText;
+           playerList = tableString.split("\n");
+           checkForPlayerInDatabase();
+           populatePlayerProfile();
+              
+           
+         }
+   }
+  xmlhttp.send();
+}
+
+/*function photoScrape() {
+  var xmlhttp = new XMLHttpRequest();
+  var url = "http://localhost:8001/photoScrape?name=" + name;
+  xmlhttp.open("GET", url, true);
+  xmlhttp.onreadystatechange = function(){
+         if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+           var photoURL = xmlhttp.responseText;
+           //alert(photoURL);
+           addPlayerPhoto(photoURL);
+           
+         }
+   }
+  xmlhttp.send();
+
+}*/
+
+/*function accessPlayerPageScraper() {
+  
+  //Calls helper that populates the profile box
+  profileScrape();
+  //Calls helper that loads a photo
+  //photoScrape();
+  
+
+  //console.log(college);
+
+
+}*/
+
+function getName() {
+  return name;
+}
+function getCollege() {
+  return college;
+}
+
+
 
--- server.js
+++ server.js
@@ -1,105 +1,186 @@
-function scrapeForPlayerNames(){
-
-  console.log("started running scrapeForPlayerNames");
-
-  /*var express = require('express');
-  var fs = require('fs');
-  var request = require('request');
-  var cheerio = require('cheerio');
-  var app = express();
-
-  app.get('/scrape', function(req, res){
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
+var http = require('http');
+var url = require('url');
+var Scraper = require("image-scraper");
 
 
-  var url = 'http://www.nbadraft.net/ranking/bigboard';
-  console.log("right before the request");
-    request(url, function(error, response, html){
-      
-      if(!error) {
-        console.log("error free request");
-        var $ = cheerio.load(html);
-        var fullname, firstname, lastname, age, position, height, weight, coll;
+var playerList = [];
+console.log("Entered server.js");
 
-        var playerList = [];
-        var currLocation;
+http.createServer(function(req, response){
+  var path = url.parse(req.url).pathname;
+  if(path == "/tableScrape"){
+    console.log("request received for TableScrape");
+    tableurl = 'http://www.nbadraft.net/ranking/bigboard';
+    request(tableurl, function(err, resp, body) {
+        //console.log("Made it into the Request");
+        if (err) {
+            throw err;
+        }
+        
+        $ = cheerio.load(body);
+        var fullname, firstname, lastname, age, position, height, weight, coll, year;
+        var currLocation, spaceIndex, length;
+        
 
-        $('.table').filter(function() {
+        //console.log("Made it right before actual scraping");
 
-          var data = $(this);
-          currLocation = data;
-          currLocation = currLocation.first();
-          currLocation = currLocation.next();
+        $('table tr').each(function(index, value) {
+          if (index == 0) {
 
+          } else {
+            currLocation = $(this).children().first().next().next();
+            fullname = currLocation.text();
+            height = currLocation.next().text();
+            weight = currLocation.next().next().text();
+            position = currLocation.next().next().next().text();
+            coll = currLocation.next().next().next().next().text();
+            year = currLocation.next().next().next().next().next().text();
 
-          //for (var i = 0; i < 20; i++) {
-            playerList.push({
-              firstname : "", 
-              lastname: "", 
-              age: "", 
-              position: "", 
-              height: "", 
-              weight: "", 
-              coll: ""
-            });
-            var currColumn = currLocation.children().first().next().next();
+            
 
-            fullname = currColumn.text();
-
-            var spaceIndex = fullname.indexOf(" ");
-            var length = fullname.length;
+            spaceIndex = fullname.indexOf(" ");
+            length = fullname.length;
 
             if (spaceIndex > 0) {
-              firstname = substring(0, spaceIndex - 1);
-              lastname = substring(spaceIndex + 1, length);
-              console.log("successfully substring-ed the full name");
+              firstname = fullname.substring(0, spaceIndex);
+              lastname = fullname.substring(spaceIndex + 1, length);
+              //console.log("successfully substring-ed the full name");
 
             } else {
-              alert("Error while parsing string in scrapeForPlayerNames in server.js");
+              console.log("Error while parsing string in scrapeForPlayerNames in server.js");
               return;
             }
 
-            arr[0].firstname = firstname;  //Replace these with i
-            arr[0].lastname = lastname;
+            playerList.push({
+              first: firstname, 
+              last: lastname, 
+              yr: year, 
+              pos: position, 
+              h: height, 
+              w: weight, 
+              col: coll
+            });
 
-            alert("The first scraped name is " + firstname + " " + lastname);
-
-
-            //currLocation = currLocation.next();
-          //}
-
-
-
-        })
-        
-
-
+            /*console.log("Pushed in " + firstname + " " + lastname + " who is " + height + " " + 
+              weight + " and plays " + position + " at " + coll + " in his " + year + " year");*/
+            //console.log(playerList[index - 1].first);
 
 
+          }
+
+          
 
 
+          });
+        //File used for debugging
+        var file = fs.createWriteStream('playersTable.txt');
+        var output = "";
+
+        //file.write("testing");
+        for (var i = 0; i < 100; i++) {
+          file.write(playerList[i].first + "," + playerList[i].last + "," + playerList[i].h + 
+            "," + playerList[i].w + "," + playerList[i].pos + "," + playerList[i].col + "," + 
+            playerList[i].yr + "\n");
+
+          output += (playerList[i].first + "," + playerList[i].last + "," + playerList[i].h + 
+            "," + playerList[i].w + "," + playerList[i].pos + "," + playerList[i].col + "," + 
+            playerList[i].yr + "\n");
+        }
+
+        file.end();
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.end(output);
+
+
+    });
+    
+
+  }
+  if (path == "/twitterScrape") {
+    console.log("request received for twitterScrape");
+
+  }
+
+  if (path == "/photoScrape") {
+    console.log("request received for photoScrape");
+    var baseURL = "https://www.google.com/search?hl=en&authuser=0&site=imghp&tbm=isch&source=hp&biw=1280&bih=679&q=";
+
+    var scraper = new Scraper(baseURL);
+    
+
+    var spaceIndex = req.url.indexOf("%");
+    var startIndex = req.url.indexOf("=");
+    var firstname = req.url.substring(startIndex + 1, spaceIndex);
+    var length = req.url.length;
+    var lastname = req.url.substring(spaceIndex + 3, length);
+    //console.log("Name is " + firstname + " " + lastname);
+
+    baseURL += firstname + "+" + lastname + "+" + "draft";
+    console.log(baseURL);
+
+    scraper.address = baseURL;
+
+    var i = 0;
+    var imageURL;
+
+    scraper.on("image", function(image) {
+      if (i == 4) {
+        //Executes this just for the first image on the page
+        imageURL = image.address;
       }
-      
-    var file = fs.createWriteStream('test.txt');
+      i++;
+    });
 
-    for(var i = 0; i < playerList.length; i++) {
-      file.write(JSON.stringify(playerList[i], null, 4));
-    }
+    console.log(imageURL);
 
-    file.end();
+    /*request(baseURL, function(err, resp, body) {
 
-
-    res.send('check your console!');
-
-    }) //;
+      if (err) {
+            throw err;
+      }
 
 
-  })
-
-  app.listen('8081')
-
-  console.log('Magic happens on port 8081');
-
-  exports = module.exports = app;*/
+        
+      $ = cheerio.load(body);
+      var photoURL = $('#rg_s, .rg_di, a').attr('href');
+      console.log(photoURL);
 
 
-}
+
+
+      response.setHeader("Access-Control-Allow-Origin", "*");
+      response.writeHead(200, {"Content-Type": "text/plain"});
+      response.end("testing");
+
+
+    });*/
+
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("testing");
+
+
+
+
+    
+
+  }
+
+
+
+}).listen(8001);
+console.log("server initialized");
+
+
+
+//For some reason playerList isn't populated out here...
+
+
+
+
+
+
